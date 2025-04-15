b4ed279d7458564573217f06e02c4d4c108b053f
MujahidulIslamShaikh <codewith247@gmail.com>
2025-04-15 03:47:20 -0700

    new file created

---
+++ config/db.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Database {
+
+    private $host = "localhost";
+    private $dbname = "test_db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+
+    public function connect(){
+        try{
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname",
+                                  $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->conn;
+        } catch(PDOException $e){
+            echo "Connection Failed !" . $e->getMessage();
+        }
+    }
+}
+
+$db = new Database();
+$db->connect();
+
+
+
+
+?>
